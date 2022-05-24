f37cc25a5d22840d9eb2f58dcc30032dba8877f1
Madalin Sandu <madalin.sandu@gmail.com>
2022-05-24 19:02:05 +0300

    Update getDB.js

--- pages/api/getDB.js
+++ pages/api/getDB.js
@@ -1,39 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get, child } from "firebase/database";
+import { query, limit, collection, orderBy, getFirestore, onSnapshot } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_xara_Vh0lEOh2sQHFmd1MzJY4qxsrAs",
-  authDomain: "everywhere-18dd2.firebaseapp.com",
-  projectId: "everywhere-18dd2",
-  storageBucket: "everywhere-18dd2.appspot.com",
-  messagingSenderId: "848673175928",
-  appId: "1:848673175928:web:e9622a35ff5c806a4ef08d"
+    apiKey: "AIzaSyD_xara_Vh0lEOh2sQHFmd1MzJY4qxsrAs",
+    authDomain: "everywhere-18dd2.firebaseapp.com",
+    projectId: "everywhere-18dd2",
+    storageBucket: "everywhere-18dd2.appspot.com",
+    messagingSenderId: "848673175928",
+    appId: "1:848673175928:web:e9622a35ff5c806a4ef08d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+function loadCoordinates() {
+    const recentMessagesQuery = query(collection(getFirestore(), 'coordinates'), orderBy('timestamp', 'desc'), limit(2));
+//console.log(recentMessagesQuery);
+    // Start listening to the query.
+    onSnapshot(recentMessagesQuery, function (snapshot) {
+        snapshot.docChanges().forEach(function (change) {
+           console.log("new coordinates saved");
+        });
+    });
+}
 
 export default (req, res) => {
-  res.statusCode = 200;
-  
-  const dbRef = ref(getDatabase(app));
+    res.statusCode = 200;
 
-  get(child(dbRef, `coordinates`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-      res.json({ message: snapshot.val() })
-    } else {
-      console.log("No data available");
-      res.json({ message: 'error' })
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
+    loadCoordinates();
+
+   res.json( {
+        message: "Hello World"
+    });
 }
